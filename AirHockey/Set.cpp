6afeb60223f124c647ab777e1cae040b9c52e14d
KerrilSonne <velikirilll@gmail.com>
2019-12-01 21:40:24 +0300

    AirHockey release v1.2
    Добавлено графическое отображение текущих настроек

--- AirHockey/Set.cpp
+++ AirHockey/Set.cpp
@@ -48,4 +48,26 @@ void Set::draw(UTFT& myGLCD) {
     myGLCD.drawCircle(280, 200, 3);
     myGLCD.drawCircle(240, 200, 3);
     myGLCD.drawCircle(200, 200, 3);
+
+    if (EEPROM.read(2) == 10) {
+      myGLCD.drawLine(170, 30, 180, 40);
+      myGLCD.drawLine(170, 50, 180, 40);
+    } else if (EEPROM.read(2) == 20) {
+      myGLCD.drawLine(170, 110, 180, 120);
+      myGLCD.drawLine(170, 130, 180, 120);
+    } else {
+      myGLCD.drawLine(170, 190, 180, 200);
+      myGLCD.drawLine(170, 210, 180, 200);
+    }
+
+    if (EEPROM.read(3) == 3) {
+      myGLCD.drawLine(10, 30, 20, 40);
+      myGLCD.drawLine(10, 50, 20, 40);
+    } else if (EEPROM.read(3) == 4) {
+      myGLCD.drawLine(10, 110, 20, 120);
+      myGLCD.drawLine(10, 130, 20, 120);
+    } else {
+      myGLCD.drawLine(10, 190, 20, 200);
+      myGLCD.drawLine(10, 210, 20, 200);
+    }
 }
